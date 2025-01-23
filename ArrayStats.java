f44e025c84aff743c33050fc5fee18e7003999d1
DaveyAgni <devarchit.agnihotri@icloud.com>
2025-01-23 09:32:34 -0500

    Update ArrayStats.java

--- ArrayStats.java
+++ ArrayStats.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//Â© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class -
@@ -14,17 +14,55 @@ public class ArrayStats
 	
 	public ArrayStats(String list)
 	{
-		//put stuff in the array	
+		setArray(list);
 	}	
 	
 	public void setArray(String list)
 	{
-		//put stuff in the array		
-	}	
+		if(list.length() % 2 == 0)
+		{
+			array = new int[list.length()/2];
+		}
+		else{
+			array= new int[list.length()/2+1];
+		}
+
+		int index = 0;
+		Scanner c = new Scanner(list);
+		while(c.hasNextInt())
+		{
+			array[index] = c.nextInt();
+			index++;
+		}
+	}
+	
 			
 	public int getNumGroupsOfSize(int size)
 	{
-		return -2938;
+		int count = 0;
+		int current = array[0];
+		int num = 0;
+		int i = 0;
+
+		for(i = 0; i < array.length; i++)
+		{
+			if(current == array[i])
+			{
+				count++;
+			}
+			else if(array[i] != current && count < size)
+			{
+				num++;
+				count = 1;
+				current = array[i];
+			}
+			else if(count >= size)
+			{
+				num++;
+			}
+		}
+
+		return num;
 	}
 	
 	public String toString()
